3f9a7b0686681bacfadb86369b31a89f529f021f
Kang-Kyu Lee <kangkyu1111@gmail.com>
2017-12-21 17:22:41 -0800

    It is not adding anything yet

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,11 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
     }
 
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    public void showThree(View view) {
+        displayForTeamA(3);
+    }
+
+    public void showTwo(View view) {
+        displayForTeamA(2);
+    }
+
+    public void showOne(View view) {
+        displayForTeamA(1);
     }
 }
